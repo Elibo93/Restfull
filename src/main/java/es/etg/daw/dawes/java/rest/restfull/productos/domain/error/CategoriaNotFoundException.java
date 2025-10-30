16307ce93dab4050d3a2cb6e6fa22723b5532ebb
Eli <elisabethbargalloortiz.aw@gmail.com>
2025-10-30 13:23:42 -0500

    domain/common

---
+++ src/main/java/es/etg/daw/dawes/java/rest/restfull/productos/domain/error/CategoriaNotFoundException.java
@@ -0,0 +1,16 @@
+package es.etg.daw.dawes.java.rest.restfull.productos.domain.error;
+
+import es.etg.daw.dawes.java.rest.restfull.common.domain.error.EntityNotFoundException;
+
+public class CategoriaNotFoundException extends EntityNotFoundException {
+
+    public static final String ENTIDAD = "categoría";
+
+    public CategoriaNotFoundException() {
+        super(ENTIDAD);
+    }
+
+    public CategoriaNotFoundException(int id) {
+        super(ENTIDAD, id);
+    }
+}
