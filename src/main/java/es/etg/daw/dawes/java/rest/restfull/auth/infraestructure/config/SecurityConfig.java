4c08f9f5f1576488a5e41e79c473142358bd2c73
Eli <elisabethbargalloortiz.aw@gmail.com>
2026-01-14 10:52:05 -0600

    auth

--- src/main/java/es/etg/daw/dawes/java/rest/restfull/auth/infraestructure/config/SecurityConfig.java
+++ src/main/java/es/etg/daw/dawes/java/rest/restfull/auth/infraestructure/config/SecurityConfig.java
@@ -4,12 +4,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 import es.etg.daw.dawes.java.rest.restfull.auth.infraestructure.db.jpa.repository.UserEntityRepository;
 import es.etg.daw.dawes.java.rest.restfull.auth.infraestructure.mapper.UserMapper;
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import lombok.RequiredArgsConstructor;
 
 @Configuration
@@ -32,6 +36,19 @@ public class SecurityConfig {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
     }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/categorias/**").hasRole("ADMIN")
+                .requestMatchers("/productos/**").hasAnyRole("USER", "ADMIN"));
+        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+
+     
 }
 
 
